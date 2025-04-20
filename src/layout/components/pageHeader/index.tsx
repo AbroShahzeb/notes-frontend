179bc4ffc329796c7e95c9200a0405e4a0ff8f5f
AbroShahzeb <khizr772@gmail.com>
2025-04-20 18:32:57 +0500

    A bit of changes

--- src/layout/components/pageHeader/index.tsx
+++ src/layout/components/pageHeader/index.tsx
@@ -14,7 +14,7 @@ export const PageHeader = ({ title }: Props) => {
       <div className="lg:hidden py-3 px-4 h-11 md:py-4 md:px-8 md:h-[74px] dark:bg-neutral-800 text-primary-text bg-neutral-100 flex items-center">
         <LogoLink />
       </div>
-      <div className="flex rounded-t-2xl bg-surface-2 items-center justify-between lg:h-[81px]  p-4 pb-0 lg:pb-8 lg:px-8 lg:border-b border-neutral-200 dark:border-neutral-800">
+      <div className="flex rounded-t-2xl lg:rounded-none bg-surface-2 items-center justify-between lg:h-[81px]  p-4 pb-0 lg:py-8 lg:px-8 lg:border-b border-neutral-200 dark:border-neutral-800">
         <h1 className="text-2xl font-bold text-primary-text dark:text-white">
           {title}
         </h1>
